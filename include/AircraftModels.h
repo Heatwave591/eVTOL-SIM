9d6a7374ce6e8743440301128755762556da2c57
Heatwave591 <anirudh.raghu591@gmail.com>
2026-08-05 14:35:42 -0500

    add rest of the aircraft info

--- include/AircraftModels.h
+++ include/AircraftModels.h
@@ -23,3 +23,36 @@ public:
     int passengerCount() const override { return 5;}
     double faultProbabilityPerHour() const override { return 0.10;}
 };
+
+class CharlieAircraft : public Aircraft {
+public:
+    std::string companyName() const override { return "Charlie Company"; }
+    double cruiseSpeedMph() const override { return 160; }
+    double batteryCapacityKwh() const override { return 220; }
+    double timeToChargeHours() const override { return 0.80; }
+    double energyUseKwhPerMile() const override { return 2.2; }
+    int passengerCount() const override { return 3; }
+    double faultProbabilityPerHour() const override { return 0.05; }
+};
+
+class DeltaAircraft : public Aircraft {
+public:
+    std::string companyName() const override { return "Delta Company"; }
+    double cruiseSpeedMph() const override { return 90; }
+    double batteryCapacityKwh() const override { return 120; }
+    double timeToChargeHours() const override { return 0.62; }
+    double energyUseKwhPerMile() const override { return 0.8; }
+    int passengerCount() const override { return 2; }
+    double faultProbabilityPerHour() const override { return 0.22; }
+};
+
+class EchoAircraft : public Aircraft {
+public:
+    std::string companyName() const override { return "Echo Company"; }
+    double cruiseSpeedMph() const override { return 30; }
+    double batteryCapacityKwh() const override { return 150; }
+    double timeToChargeHours() const override { return 0.30; }
+    double energyUseKwhPerMile() const override { return 5.8; }
+    int passengerCount() const override { return 2; }
+    double faultProbabilityPerHour() const override { return 0.61; }
+};
