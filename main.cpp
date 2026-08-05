ff3fa69bfd30c96b907cd1f6ff172b7418038351
Heatwave591 <anirudh.raghu591@gmail.com>
2026-08-05 16:23:13 -0500

    moved to unique_ptr

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include "AircraftModels.h"
+#include <memory>
 
 int main(){
-    Aircraft* alpha = new AlphaAircraft();
+    std::unique_ptr<Aircraft> alpha = std::make_unique<AlphaAircraft>();
 
     std::cout << alpha->companyName() << ": "
               << alpha->flightDurationHours() << " hr, "
               << alpha->flightDistanceMiles() << " mi\n";
 
-    delete alpha;
     return 0;
 }
