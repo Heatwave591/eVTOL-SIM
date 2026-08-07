cc9ac0573ba50597e8fbef988413687c9667e5a3
Heatwave591 <anirudh.raghu591@gmail.com>
2026-08-06 23:34:18 -0500

    adde vehicle class and stats

---
+++ include/Vehicle.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <memory>
+#include "Aircraft.h"
+
+
+enum class VehicleState {FLying, WaitingForCharger, Charging};
+
+struct VehicleStats{
+    int flightCount = 0;
+    double totalFlightHours = 0;
+    double totalDistanceMiles = 0;
+    int chargeCount = 0;
+    double totalChargeHours = 0;
+    int faultCount = 0;
+};
+
+class Vehicle{
+    public:
+        Vehicle(int id, std::unique_ptr<Aircraft> aircraft) : id_(id), aircraft_(std::move(aircraft)) {}
+
+        int id() const { return id_; }
+        const Aircraft& aircraft() const { return *aircraft_; }
+        VehicleState state() const { return state_; }
+        void setState(VehicleState s) { state_ = s; }
+        const VehicleStats& stats() const { return stats_; }
+
+        void recordFlightCompleted(double durationHours, double distanceMiles){
+            stats_.chargeCount++;
+            stats_.totalChargeHours += durationHours;
+        }
+
+        void recordChargeCompleted(double durationHours) {
+        stats_.chargeCount++;
+        stats_.totalChargeHours += durationHours;
+    }
+    
+    void recordFaults(int count) { stats_.faultCount += count; }
+
+
+    private:
+        int id_;
+        std::unique_ptr<Aircraft> aircraft_;
+        VehicleState state_ = VehicleState::FLying;
+        VehicleStats stats_;
+};
--- main.cpp
+++ main.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 #include "AircraftModels.h"
+#include "Vehicle.h"
 #include <memory>
 
 int main(){
-    std::unique_ptr<Aircraft> alpha = std::make_unique<AlphaAircraft>();
+    // std::unique_ptr<Aircraft> alpha = std::make_unique<AlphaAircraft>();
 
-    std::cout << alpha->companyName() << ": "
-              << alpha->flightDurationHours() << " hr, "
-              << alpha->flightDistanceMiles() << " mi\n";
+
+    // std::cout << alpha->companyName() << ": "
+    //           << alpha->flightDurationHours() << " hr, "
+    //           << alpha->flightDistanceMiles() << " mi\n";
+
+    Vehicle v(0, std::make_unique<AlphaAircraft>());
+
+    std::cout << v.aircraft().companyName() << ": "
+              << v.aircraft().flightDurationHours() << "hrs, "
+              << v.aircraft().flightDistanceMiles() << "mi\n";
 
     return 0;
 }
