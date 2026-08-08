ff79dee8ef6d60b2de0d4ac91e910f6b0beac78b
Heatwave591 <anirudh.raghu591@gmail.com>
2026-08-07 19:48:06 -0500

    Create charging station and fifo charger distribution

---
+++ include/ChargingStation.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <queue>
+#include <optional>
+
+using namespace std;
+
+class ChargingStation{
+public:
+    explicit ChargingStation(int chargerCount) : availableChargers_(chargerCount){}
+
+    size_t waitingCount() const { return waitingQueue_.size(); }
+    int availableChargers() const { return availableChargers_; }
+
+    bool requestCharger(int vehicleId){
+        if(availableChargers_ > 0){
+            --availableChargers_;
+            return true;
+        }
+        waitingQueue_.push(vehicleId);
+        return false;
+    }
+
+    optional<int> releaseCharger(){
+        if(!waitingQueue_.empty()){
+            int newVehicle = waitingQueue_.front();
+            waitingQueue_.pop();
+            return newVehicle;
+        }
+        ++availableChargers_;
+        return nullopt;
+    } 
+
+
+private:
+    int availableChargers_;
+    queue<int> waitingQueue_;
+};
