31c428f1199db61ab7e2592e2df2eef7dd3519f4
Heatwave591 <anirudh.raghu591@gmail.com>
2026-08-08 13:06:25 -0500

    shit is getting complex, so we will retain this as our last checkpoint

--- include/Event.h
+++ include/Event.h
@@ -1,6 +1,6 @@
 #pragma once
 
-enum class EventType {FightComplete, ChargeComplete};
+enum class EventType {FlightComplete, ChargeComplete};
 
 struct Event{
     double time;
@@ -9,5 +9,5 @@ struct Event{
 };
 
 struct EventComparator{
-    bool operator()(const Event& a, Event& b) const { return a.time < b.time; };
+    bool operator()(const Event& a, const Event& b) const { return a.time < b.time; };
 };
